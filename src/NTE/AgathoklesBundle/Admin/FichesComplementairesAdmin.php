8fccb267fa4508f7b14bbcf64f6b639c1e19cc82
Alrick <alrick.deillon@unifr.ch>
2015-03-30 15:49:02 +0200

    revert fiches admin

--- src/NTE/AgathoklesBundle/Admin/FichesComplementairesAdmin.php
+++ src/NTE/AgathoklesBundle/Admin/FichesComplementairesAdmin.php
@@ -14,5 +14,47 @@ use NTE\AgathoklesBundle\Entity\FichesComplementaires;
 
 class FichesComplementairesAdmin extends Admin
 {
-    
+    // LIST FIELDS
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('fiche')
+            ->add('media', 'sonata_type_model', array('template' => 'NTEAgathoklesBundle:Fiches:crudimage.html.twig'))
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'view' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+           ))
+        ;
+    }
+
+    // FORM FIELDS
+    /**
+     * Create and Edit
+     * @param \Sonata\AdminBundle\Form\FormMapper $formMapper
+     *
+     * @return void
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+#            ->add('fiche')
+            ->add('ficheComplementaire')
+            ->add('media', 'sonata_type_model_list', array('required' => false), array('link_parameters' => array('context' => 'default', 'provider'=>'sonata.media.provider.image')))
+            ->end()
+        ;
+    }
+
+    // SHOW FIELDS
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('fiche')
+            ->add('ficheComplementaire')
+            ->add('media', 'sonata_type_model', array('template' => 'NTEAgathoklesBundle:Fiches:crudimage.html.twig'))
+            ->end()
+        ;
+    }
 }
