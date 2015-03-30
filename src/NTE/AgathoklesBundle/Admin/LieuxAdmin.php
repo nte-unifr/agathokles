9dd4a2bb2c848763b2993abdd80276cce5abd674
Alrick <alrick.deillon@unifr.ch>
2015-03-30 16:26:49 +0200

    remove map from lieux in admin

--- src/NTE/AgathoklesBundle/Admin/LieuxAdmin.php
+++ src/NTE/AgathoklesBundle/Admin/LieuxAdmin.php
@@ -24,6 +24,8 @@ class LieuxAdmin extends Admin
     {
         $listMapper
             ->addidentifier('nom')
+            ->add('lat')
+            ->add('lng')
             ->add('_action', 'actions', array(
                 'actions' => array(
                     'edit' => array(),
@@ -59,66 +61,6 @@ class LieuxAdmin extends Admin
                 ->add('nom')
                 ->add('lat', null, array('read_only' => false, 'attr' => array('class' => 'latID')))
                 ->add('lng', null, array('read_only' => false, 'attr' => array('class' => 'lngID')))
-                ->setHelps(array(
-                    'nom' => "Utilisez la carte ci-dessous pour définir l'emplacement géographique du lieu.<div id=\"map\"></div>
-
-                                <script type=\"text/javascript\">
-                                    var lat;
-                                    var lng;
-                                    var marker;
-
-                                    var map = L.map('map', {attributionControl: true}).setView([37.05, 7.16], 5);
-                                    map.setMaxBounds([[59.689926, -9.239258], [25.363882, 46.538086]]);
-
-                                    L.tileLayer('/agathokles/bundles/nteagathokles/maps/{z}/{x}/{y}.png', {
-                                        maxZoom: 8,
-                                        minZoom: 5,
-                                        attribution: 'Carte: OpenStreetMap MapQuest',
-                                    }).addTo(map);
-
-                                    function onMapClick(e) {
-                                        if ( null == marker ) {
-                                            $('.latID').val(e.latlng['lat']);
-                                            $('.lngID').val(e.latlng['lng']);
-
-                                            marker = new L.marker(e.latlng, {draggable:'true'});
-                                            marker.on('dragend', function(event){
-                                                    var marker = event.target;
-                                                    var position = marker.getLatLng();
-                                                    $('.latID').val(position['lat']);
-                                                    $('.lngID').val(position['lng']);
-                                                    //alert(position);
-                                            });
-                                            map.addLayer(marker);
-                                        }
-                                    }
-
-                                    map.on('click', onMapClick);
-
-                                    $( document ).ready(function() {
-                                        lat = $('.latID').val();
-                                        lng = $('.lngID').val();
-                                        //alert( 'lat = ' + lat + ' et lng = ' + lng );
-
-                                        if ( '' != lat && '' != lng ) {
-                                            //alert(lat + ' et ' + lng);
-                                            marker = L.marker([lat, lng], {draggable:'true'}).addTo(map);
-                                            var position = marker.getLatLng();
-                                            marker.on('dragend', function(event){
-                                                    var marker = event.target;
-                                                    var position = marker.getLatLng();
-                                                    //alert(position);
-                                                    $('.latID').val(position['lat']);
-                                                    $('.lngID').val(position['lng']);
-                                            });
-                                            var position = marker.getLatLng();
-                                            map.setView([position['lat'], position['lng']], 5);
-                                        }
-                                    });
-                                </script>
-                                    ",
-                ))
-#            ->add('fiches')
             ->end()
         ;
     }
