c49d1ebe76592c9972da28635d3a4a075bb0f31b
Alrick <alrick.deillon@unifr.ch>
2015-04-20 16:41:11 +0200

    init masonry only on timbres index page

--- src/NTE/AgathoklesBundle/Resources/public/js/main.js
+++ src/NTE/AgathoklesBundle/Resources/public/js/main.js
@@ -1,8 +1,10 @@
 $(function() {
-    // initialize Masonry
-    var $container = $('.js-masonry').masonry();
-    // layout Masonry again after all images have loaded
-    $container.imagesLoaded( function() {
-      $container.masonry();
-    });
+    if ($(".js-masonry").length) {
+        // initialize Masonry
+        var $container = $(".js-masonry").masonry();
+        // layout Masonry again after all images have loaded
+        $container.imagesLoaded( function() {
+          $container.masonry();
+        });
+    }
 });
