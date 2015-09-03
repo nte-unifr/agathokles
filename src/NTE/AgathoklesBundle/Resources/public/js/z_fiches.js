eb1ffdeaf7b6f807cd22e96d67160dff7facca83
Alrick <alrick.deillon@unifr.ch>
2015-09-03 09:12:46 +0200

    masonryjs + grid for fiche
    switch from salvatorejs to masonryjs and implement cascading grid for
    fiche view

--- src/NTE/AgathoklesBundle/Resources/public/js/z_fiches.js
+++ src/NTE/AgathoklesBundle/Resources/public/js/z_fiches.js
@@ -1,5 +1,12 @@
-$(function() {
-
+$(document).ready(function() {
     // little hack to hide timbres label of lieu select filter
     $("label[for='filter_fiches_timbres_0_lieu']").hide();
+
+    $('.masonry-grid').masonry({
+      // options
+      itemSelector: '.masonry-grid-item',
+      columnWidth: '.masonry-grid-sizer',
+      percentPosition: true,
+      gutter: 10
+    });
 });
