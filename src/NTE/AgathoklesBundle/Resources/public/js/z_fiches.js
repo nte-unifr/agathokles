7099513a6ea3967426229cf2c54e248a70977545
Alrick <alrick.deillon@unifr.ch>
2016-06-28 11:29:36 +0200

    add link from index and fix masonry on show

--- src/NTE/AgathoklesBundle/Resources/public/js/z_fiches.js
+++ src/NTE/AgathoklesBundle/Resources/public/js/z_fiches.js
@@ -7,12 +7,28 @@ function resizeImgContainer () {
 }
 
 $(document).ready(function () {
-  // little hack to hide timbres label of lieu select filter
-  $("label[for='filter_fiches_timbres_0_lieu']").hide()
+  // index
+  if ($('#fiches').length) {
+    // little hack to hide timbres label of lieu select filter
+    $("label[for='filter_fiches_timbres_0_lieu']").hide()
 
-  $('#fiches').imagesLoaded(function () {
-    resizeImgContainer()
-  })
+    $('#fiches').imagesLoaded(function () {
+      resizeImgContainer()
+    })
 
-  $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="tooltip"]').tooltip()
+  }
+
+  // show
+  if ($('#fiche').length) {
+    $('#fiche').imagesLoaded(function () {
+      $('.masonry-grid').masonry({
+        // options
+        itemSelector: '.masonry-grid-item',
+        columnWidth: '.masonry-grid-sizer',
+        percentPosition: true,
+        gutter: 10
+      })
+    })
+  }
 })
