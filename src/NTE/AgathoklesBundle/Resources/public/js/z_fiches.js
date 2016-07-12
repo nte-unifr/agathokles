0e22c003d7400099626bcfa71f0917387b5a6050
Alrick <alrick.deillon@unifr.ch>
2016-07-12 10:00:56 +0200

    new images placeholder

--- src/NTE/AgathoklesBundle/Resources/public/js/z_fiches.js
+++ src/NTE/AgathoklesBundle/Resources/public/js/z_fiches.js
@@ -1,21 +1,10 @@
 /* global $ */
 
-function resizeImgContainer () {
-  var img = $('#fiches img').first()
-  var height = img.height()
-  $('#fiches .img-placeholder').height(height)
-}
-
 $(document).ready(function () {
   // index
   if ($('#fiches').length) {
     // little hack to hide timbres label of lieu select filter
     $("label[for='filter_fiches_timbres_0_lieu']").hide()
-
-    $('#fiches').imagesLoaded(function () {
-      resizeImgContainer()
-    })
-
     $('[data-toggle="tooltip"]').tooltip()
   }
 
